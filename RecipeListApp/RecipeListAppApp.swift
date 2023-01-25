5dae9cbfb132deb5cd2e358f70dad6fbf3a1e45f
gravilaxe <joshuawright3132@outlook.com>
2023-01-25 14:58:38 +0000

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Joshua Wright on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
